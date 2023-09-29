08403e6114d621ad18861a3ced8b3ac2f554623b
Rion Tuscano <riontuscano@gmail.com>
2023-09-29 11:17:13 +0530

    123

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// console.log("Testing..............")
+b1=document.getElementById("notes")
+b1.addEventListener("click",()=>{
+    console.log("Testing..............")
+    // document.getElementById("notes").innerHTML.href = "media/onlieform.pdf"
+    document.getElementById("display_n_yr1").setAttribute("src", "/media/onlieform.pdf");
+    // document.getElementById("display_n_yr1").innerHTML.search.replace = ("#","/media/onlieform.pdf")
+},false)
